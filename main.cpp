b3b463cf358c8d63e4d0824a527616c0563dbefe
GoForBroke <go.for.broke1006@gmail.com>
2018-10-11 01:56:02 +0300

    Start parse script file

--- main.cpp
+++ main.cpp
@@ -1,14 +1,44 @@
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
 
+using namespace std;
 using namespace cv;
+namespace pt = boost::property_tree;
 
 int main(int argc, char **argv) {
+    if (2 != argc) {
+        cerr << "Usage: \nslight-animation <script filename>" << endl;
+        return -1;
+    }
 
-    std::string assetsDir = "/home/goforbroke/CLionProjects/slight-animation/assets/";
+    std::string scriptFilename = argv[1];
 
-    VideoWriter video("out.avi", CV_FOURCC('M', 'J', 'P', 'G'), 10, Size(400, 300), true);
-    for (int i = 0; i < 100; ++i) {
+    pt::ptree root;
+    pt::read_json(scriptFilename, root);
+
+    int width = root.get<int>("width", 0);
+    int height = root.get<int>("height", 0);
+    string outputFilename = root.get<char *>("output-file", 0);
+
+    VideoWriter video(outputFilename, CV_FOURCC('M', 'J', 'P', 'G'), 10, Size(width, height), true);
+
+    for (pt::ptree::value_type &animation : root.get_child("animations")) {
+        cout << animation.second.get_child("clip").data() << endl;
+
+        cout << animation.second.get_child("start").get_child("frame").data() << endl;
+        cout << animation.second.get_child("start").get_child("position").get_child("x").data() << endl;
+        cout << animation.second.get_child("start").get_child("position").get_child("y").data() << endl;
+
+        cout << animation.second.get_child("stop").get_child("frame").data() << endl;
+        cout << animation.second.get_child("stop").get_child("position").get_child("x").data() << endl;
+        cout << animation.second.get_child("stop").get_child("position").get_child("y").data() << endl;
+
+        cout << endl;
+    }
+
+    /*for (int i = 0; i < 100; ++i) {
         Mat dst = imread(assetsDir + "sample-1.jpg", IMREAD_COLOR);
 
         Mat src2;
@@ -28,9 +58,8 @@ int main(int argc, char **argv) {
             src2 = imread(assetsDir + "4.png", CV_LOAD_IMAGE_COLOR);
             addWeighted(dst, 1.0, src2, 1.0, 0.0, dst);
         }
-
-        //dst.convertTo(dst, CV_8UC1);
         video.write(dst);
-    }
+    }*/
+
     return 0;
 }
